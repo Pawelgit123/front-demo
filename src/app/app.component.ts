c374ba92c4ff9c019c4ee77489bfb903d080843c
Pawelgit123 <60837096+Pawelgit123@users.noreply.github.com>
2021-02-27 13:09:44 +0100

    fix commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Localisation} from './model/localisation';
+import {HttpClient} from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'weatherprojektang';
+export class AppComponent implements OnInit{
+  title = 'Moje lokalizacje:';
+  localisations: Localisation[];
+  constructor(private httpclient: HttpClient) {
+  }
+  ngOnInit(): void {
+    this.httpclient.get<Localisation[]>('http://localhost:8080/localisation')
+      .subscribe(response => this.localisations = response);
+  }
+
 }
+
---
+++ src/app/model/localisation.ts
@@ -0,0 +1,8 @@
+export class Localisation{
+  cityName: string;
+  region?: string;
+  country: string;
+  longitude: number;
+  latitude: number;
+}
+
